9505d79f8b96bf8d22301cd5556d836ee7d5b9b7
Sayak Sengupta <sayaksengupta74@gmail.com>
2022-04-23 01:54:37 +0530

    added main server file and connected db

---
+++ server/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const socketio = require("socket.io");
+const http = require("http");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("./db/conn");
+app.use(cookieParser());
+const connection = require("./db/conn");
+app.use(express.urlencoded({ extended: false }));
+app.use(require("./router/routes"));
+const port = process.env.PORT || 8000;
+
+
+connection();
+
+const server = http.createServer(app);
+
+
+
+server.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
