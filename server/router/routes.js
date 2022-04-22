a084b82f209ec6271c92428d73b759135111c7e4
Sayak Sengupta <sayaksengupta74@gmail.com>
2022-04-23 04:32:42 +0530

    added posting Questions feature and getPosts of all categories

--- server/router/routes.js
+++ server/router/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Queries = require("../models/queries");
 const bcrypt = require("bcrypt");
 const auth = require("../middleware/auth");
 const answer = require("../middleware/answer");
@@ -100,6 +101,30 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/post-question", async (req, res) => {
+  try {
+    const { question, category, username, answers, profilePic } = req.body;
+
+    if (!question || !category || !username || !profilePic) {
+      return res.status(422).json({ error: "Please fill all the fields." });
+    } else {
+      const queries = new Queries({
+        question,
+        category,
+        username,
+        answers,
+        profilePic,
+      });
+
+      const postedQuestion = await queries.save();
+
+      res.status(201).json(queries);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 
 router.get("/logout", auth, async (req, res) => {
   try {
@@ -112,6 +137,104 @@ router.get("/logout", auth, async (req, res) => {
     await req.rootUser.save();
   } catch (e) {
     res.status(500).send(e);
+  }
+});
+
+router.get("/getusers", async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.send(allUsers);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.post("/getQueries-for-user", async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    const user = await User.findOne({ username: username });
+
+    const categoryQueries = await Queries.find({
+      $or: [
+        { category: user.category[0] },
+        { category: user.category[1] },
+        { category: user.category[2] },
+        { category: user.category[3] },
+        { category: user.category[4] },
+      ],
+    }).sort({ date: -1 });
+
+    console.log(categoryQueries);
+    res.send(categoryQueries);
+
+    res.send(categoryQueries);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.get("/getQueries", async (req, res) => {
+  try {
+    const allQueries = await Queries.find();
+    res.send(allQueries);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.get("/get-health-queries", async (req, res) => {
+  try {
+    const healthQueries = await Queries.find({ category: "health" }).sort({
+      date: -1,
+    });
+    res.send(healthQueries);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.get("/get-business-queries", async (req, res) => {
+  try {
+    const businessQueries = await Queries.find({ category: "business" }).sort({
+      date: -1,
+    });
+    res.send(businessQueries);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.get("/get-lifestyle-queries", async (req, res) => {
+  try {
+    const lifestyleQueries = await Queries.find({ category: "lifestyle" }).sort(
+      { date: -1 }
+    );
+    res.send(lifestyleQueries);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.get("/get-education-queries", async (req, res) => {
+  try {
+    const educationQueries = await Queries.find({ category: "education" }).sort(
+      { date: -1 }
+    );
+    res.send(educationQueries);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.get("/get-trending-queries", async (req, res) => {
+  try {
+    const trendingQueries = await Queries.find({ category: "trending" }).sort({
+      date: -1,
+    });
+    res.send(trendingQueries);
+  } catch (e) {
+    res.send(e);
   }
 });
 
